33a31895c2218a223a26a1f7dcaf3e63f194c5b8
sonysn <stephen.nyamali@gmail.com>
2023-06-15 03:08:25 +0100

    fix

--- lib/models/requested_rides.dart
+++ lib/models/requested_rides.dart
@@ -1,28 +1,45 @@
-// class RequestedRides {
-//   final String requestDate;
-//   final String requestTime;
-//   final String pickupAddress;
-//   final String dropOffAddress;
-//   final int fare;
-//   final String status;
-//
-//   RequestedRides({
-//     required this.requestDate,
-//     required this.requestTime,
-//     required this.pickupAddress,
-//     required this.dropOffAddress,
-//     required this.fare,
-//     required this.status
-//   });
-//
-//   factory RequestedRides.fromJson(Map json){
-//     return RequestedRides(
-//       requestDate:,
-//       requestTime:,
-//       pickupAddress: ,
-//       dropOffAddress: ,
-//       fare: ,
-//       status: ,
-//     );
-//   }
-// }
+/// A class representing a requested ride.
+class RequestedRides {
+  /// The date the ride was requested.
+  final String requestDate;
+
+  /// The pickup address for the ride.
+  final String pickupAddress;
+
+  /// The drop-off address for the ride.
+  final String dropOffAddress;
+
+  /// The fare for the ride.
+  final int fare;
+
+  /// The status of the ride.
+  final String status;
+
+  /// Creates a new instance of the [RequestedRides] class.
+  RequestedRides({
+    required this.requestDate,
+    required this.pickupAddress,
+    required this.dropOffAddress,
+    required this.fare,
+    required this.status,
+  });
+
+  /// Creates a new instance of the [RequestedRides] class from a JSON map.
+  ///
+  /// The [json] parameter should contain the following keys:
+  ///
+  /// - `REQUEST_DATE`: A string representing the date the ride was requested.
+  /// - `PICKUP_LOCATION`: A string representing the pickup address.
+  /// - `DROPOFF_LOCATION`: A string representing the drop-off address.
+  /// - `FARE`: An integer representing the fare for the ride.
+  /// - `STATUS`: A string representing the status of the ride.
+  factory RequestedRides.fromJson(Map<String, dynamic> json) {
+    return RequestedRides(
+      requestDate: json['REQUEST_DATE'],
+      pickupAddress: json['PICKUP_LOCATION'],
+      dropOffAddress: json['DROPOFF_LOCATION'],
+      fare: json['FARE'],
+      status: json['STATUS'],
+    );
+  }
+}
